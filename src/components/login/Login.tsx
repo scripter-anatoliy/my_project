655ccb587f88baac9859e52550d816f7a7d5c9f0
alex170485 <alex.170485@icloud.com>
2021-04-30 22:53:20 +0300

    adding jsx to login component. create useState to login input and password input, create function to checkbox

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
+import styles from "login.module.css"
+
+
+
 
 export const Login = () => {
+    const [loginValue, setLoginValue] = useState('')
+    const [passwordValue, setPasswordValue] = useState('')
+    const rememberMeHandler = () => {
+        console.log('заглушка')
+    }
+
     return (
         <div>
             <h1>Login</h1>
+            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
+                <div style={{display: "flex", flexDirection: 'column', alignItems: 'center'}}>
+                    <input type='text' value={loginValue}/> Login
+                    <input type='password' value={passwordValue}/> Password
+                    <input type='checkbox' onChange={rememberMeHandler}/> Remember Me
+                </div>
+                <div>
+                    <button>Login</button>
+                </div>
+            </div>
+
         </div>
     )
 }
