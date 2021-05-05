d5278c8bc1580eff4ca65bf9a6bf4d3c508ffe97
Anatoly <anatoly.selivanov1@gmail.com>
2021-05-05 22:14:51 +0300

    refactor error

--- src/utils/validators/validators.tsx
+++ src/utils/validators/validators.tsx
@@ -1,13 +1,12 @@
 export const required = (value: string) => {
     if (value)
         return undefined
-
     return "Field is required"
 }
 
 
 export const minLengthCreator = (minLength: string | number) => (value: string) => {
     if (value.length < minLength)
-        return `Max length ${minLength} symbols`
+        return `Min length ${minLength} symbols`
     return undefined
 }
