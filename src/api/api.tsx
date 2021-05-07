9e734b6e09e2059b0d1292520c1e9dfb4281fd1a
nickolayArbuzov <56513866+nickolayArbuzov@users.noreply.github.com>
2021-05-07 17:38:32 +0300

    feat: update url-server

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     withCredentials: true,
-    baseURL: "http://localhost:7542/2.0/"
-
+    baseURL: "https://neko-back.herokuapp.com/2.0/",
 })
 
 export const authAPI = {
